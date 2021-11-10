0573eb6d65ed802004dc225707c2dd67c0795d73
Carolina Hoeflich <carohoeflich@hotmail.com>
2021-11-09 20:45:12 -0600

    Data types in Javascript

---
+++ sesion-01/app.js
@@ -0,0 +1,35 @@
+/**************PRIMITIVES****************/
+//*******Strings**********/
+var nombre = "Caro";
+var edad = 25;
+var texto = "Esto es texto";
+//*********Numbers*********/
+var numero = 234234234;
+//*******Booleans*********/
+var verdadero = true;
+var falso = false;
+
+
+
+
+/*************COMPOUNDS**************/
+/*** Objects ***/
+var persona = {
+    //property: value
+    name: "caro",
+    age:25,
+    woman: true
+};
+
+/*****Arrays******/
+var namesOfStudents = [name1, name2, name3];
+
+/*****Functions*****/
+var suma = function (num1,num2){
+    return num1 + num2;
+}
+
+
+/**********************SPECIAL ONES**************/
+var numero = undefined; //not defined
+var numero2= null; //declared but empty
